fa3ef8ac1d84479adc8eb96904efdbd0f844559c
Ivan Petrakov <rebyatyonok@yandex.com>
2022-08-20 14:01:45 +0300

    feat: todo items router

---
+++ src/server/router/todoItems.ts
@@ -0,0 +1,58 @@
+import { createRouter } from './context';
+import { z } from 'zod';
+
+export const todoItemsRouter = createRouter()
+  .query('list', {
+    input: z
+      .object({
+        take: z.number().optional(),
+        skip: z.number().optional(),
+      })
+      .strict(),
+    async resolve({ ctx, input }) {
+      return ctx.prisma.todoItem.findMany(input);
+    },
+  })
+  .mutation('create', {
+    input: z
+      .object({
+        title: z.string(),
+        text: z.string().optional(),
+        expiresAt: z.date(),
+      })
+      .strict(),
+    async resolve({ ctx, input }) {
+      return ctx.prisma.todoItem.create({
+        data: input,
+      });
+    },
+  })
+  .mutation('update', {
+    input: z
+      .object({
+        id: z.string(),
+        title: z.string().optional(),
+        text: z.string().optional(),
+        expiresAt: z.date().optional(),
+        done: z.boolean().optional(),
+      })
+      .strict(),
+    async resolve({ ctx, input }) {
+      return ctx.prisma.todoItem.update({
+        where: { id: input.id },
+        data: input,
+      });
+    },
+  })
+  .mutation('delete', {
+    input: z
+      .object({
+        id: z.string(),
+      })
+      .strict(),
+    async resolve({ ctx, input }) {
+      return ctx.prisma.todoItem.delete({
+        where: input,
+      });
+    },
+  });
