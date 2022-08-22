e175f1e5a99b7dc98bdd6f958501b18f0d5a89e0
Ivan Petrakov <rebyatyonok@yandex.com>
2022-08-22 19:56:28 +0300

    fix: a tag to next/link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from 'next';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
 	return (
 		<section>
 			<p>This is a default page, and you are not supposed to see this</p>
 			<p>
-				<a href="/todo" className='text-blue-600 visited:text-purple-600'>Go to the Todo list</a>
+				<Link href="/todo" className='text-blue-600 visited:text-purple-600'>Go to the Todo list</Link>
 			</p>
 		</section>
 	);
